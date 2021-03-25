ad3fde9b05896365512fde13631e889b1aa68d36
Kevin Dalton <kevinmdalton@gmail.com>
2021-03-25 13:33:55 -0400

    i have a working example now

---
+++ assign.py
@@ -0,0 +1,102 @@
+from diffgeolib import *
+from tqdm import trange,tqdm
+from matplotlib import pyplot as plt
+import numpy as np
+import reciprocalspaceship as rs
+import gemmi
+
+from IPython import embed
+from sys import exit
+
+# These are the hyperparameters of the indexer =>
+lam_min,lam_max = 0.95, 1.3
+dmin = 1.4
+rlp_radius = 0.002
+nsteps = 10
+# <= These are the hyperparameters of the indexer
+
+ii_file  = "data/e080_001.mccd.ii"
+inp_file = "data/e080_001.mccd.inp"
+
+expt_file = "data/split_000.expt" #<= output from the scan varying model we shipped to derek. 
+# ^^using goniometer maths, this could be replaced with the output of dials.index.
+refl_file = "data/strong.refl" #<= `dials.find_spots {image_path}/*_001.mccd gain=0.1`
+
+ds = rs.read_precognition(ii_file).reset_index()
+
+
+from dxtbx.model.experiment_list import ExperimentListFactory
+from dials.array_family.flex import reflection_table
+refls = reflection_table.from_file(refl_file)
+train_pixpos = refls['xyzobs.px.value'].as_numpy_array()[:,:2]
+
+elist = ExperimentListFactory.from_json_file(expt_file)
+c = elist.crystals()[0]
+uc = c.get_unit_cell()
+ds.spacegroup = gemmi.SpaceGroup(c.get_space_group().type().universal_hermann_mauguin_symbol())
+ds.cell = gemmi.UnitCell(*uc.parameters())
+
+# Just sort and filter the integrated spots from precognition to match `refls`
+# Note that this isn't perfect and sometimes it might happen that the rows in `refls` get assigned to the same entry in `ds`
+pixpos = ds[['X', 'Y']].to_numpy(np.float32)
+idx = np.argmin(np.linalg.norm(train_pixpos[:,None] - pixpos, axis=-1), axis=-1)
+ds = ds.iloc[idx]
+pixpos = ds[['X', 'Y']].to_numpy(np.float32)
+hkl = ds[['H', 'K', 'L']].to_numpy(np.float32).T
+
+"""
+RB = np.array(c.get_A()).reshape((3,3)) returns the reciprocal basis vectors
+
+Reciprocal space basis vectors
+------------------------
+astar123 = [0]
+bstar123 = [1]
+cstar123 = [2]
+"""
+RB = np.array(c.get_A()).reshape((3,3))
+O = np.array(uc.orthogonalization_matrix()).reshape((3,3))
+B = np.linalg.inv(O).T
+R = RB@np.linalg.inv(B)
+
+# Detector.from_expt_file returns a generator of detector panels
+detector = next(Detector.from_expt_file(expt_file))
+
+s0 = np.array([0., 0., -1.])
+s1 = detector.pix2lab(pixpos)
+s1 = s1/np.linalg.norm(s1, axis=1)[:,None]
+Q = (s1 - s0)
+
+# Here we are going to compute the miller indices implied by dials's choice of 
+# reciprocal bases. Then we are going to update the indexing solution from precog
+# to match it.
+lam = ds.Wavelength.to_numpy(np.float32)
+href = (np.linalg.pinv(RB)@(Q.T/lam[None,:])).T
+# ^^Technically you should just be able to use use np.linalg.inv but sometimes it gives me trouble so I just used the pseudo-inverse here for safety.
+
+#Put the precog output into the indexing solution chosen by dials
+hkl = align_hkls(href, hkl.T, ds.spacegroup)
+assert np.allclose(href, hkl, atol=3.)
+
+abc = np.linalg.norm(O, axis=0)
+hmax = np.floor(abc/dmin)
+
+la = LaueAssigner(s0, s1, lam_min, lam_max, hmax, RB)
+
+plt.figure()
+plt.title("Millers Before Optimization")
+correct = is_ray_equivalent(hkl, la.H)
+plt.plot(pixpos[correct,0], pixpos[correct,1], 'k.', label='Correct')
+plt.plot(pixpos[~correct,0], pixpos[~correct,1], 'r.', label='Inorrect')
+plt.legend()
+
+la.optimize_bases(rlp_radius, nsteps)
+
+plt.figure()
+correct = is_ray_equivalent(hkl, la.H)
+plt.title("Millers After Optimization")
+plt.plot(pixpos[correct,0], pixpos[correct,1], 'k.', label='Correct')
+plt.plot(pixpos[~correct,0], pixpos[~correct,1], 'r.', label='Inorrect')
+plt.legend()
+
+plt.show()
+
--- diffgeolib.py
+++ diffgeolib.py
@@ -7,6 +7,37 @@ import gemmi
 This is just a place to stash useful functions and classes for diffraction geometry calculations
 """
 
+def hkl2ray(hkl, wavelength=None):
+    """ 
+    Convert a miller index to the shortest member of its central ray. 
+    Optionally, adjust its wavelength accordingly.
+
+    Parameters
+    ----------
+    hkl : array
+        `n x 3` array of miller indices. the dtype must be interpretable as an integeger
+    wavelength : array (optional)
+        length `n` array of wavelengths corresponding to each miller index
+
+    Returns
+    -------
+    reduced_hkl : array
+        The miller index of the shortest vector on the same central ray as hkl
+    reduced_wavelength : array (optional)
+        The wavelengths corresponding to reduced_hkl
+    """ 
+    gcd = np.gcd.reduce(hkl.astype(int), axis=1)
+    if wavelength is not None:
+        return hkl/gcd[:,None], wavelength*gcd
+    else:
+        return hkl/gcd[:,None]
+
+def is_ray_equivalent(hkl1, hkl2):
+    """
+    Test for equivalency between two miller indices in a Laue experiment. Returns a boolean array for each of the `n` hkls in `hkl{1,2}`.
+    """
+    return np.all(np.isclose(hkl2ray(hkl1),  hkl2ray(hkl2)), axis=1)
+
 def align_hkls(reference, target, spacegroup, anomalous=True):
     """
     Use the point group operators in `spacegroup` to align target hkls to reference.    
@@ -80,6 +111,9 @@ class Detector():
 
     @classmethod
     def from_expt_file(cls, filename, **kwargs):
+        """
+        Return a generator of Detector instances for each panel described in `filename`
+        """
         from dxtbx.model.experiment_list import ExperimentListFactory
         elist = ExperimentListFactory.from_json_file(filename)
         for detector in elist.detectors():
@@ -91,6 +125,7 @@ class Detector():
                 yield cls(ax1, ax2, ori, **kwargs)
 
     def pix2lab(self, xy, D=None):
+        """ Map xy to 3d coordinates in the lab frame in mm"""
         if D is None:
             D = self.D
         xy = np.pad(xy, ((0,0), (0,1)), constant_values=1.)
@@ -98,12 +133,14 @@ class Detector():
         return S1
 
     def lab2pix(self, xyz, D=None):
+        """ Map 3d coordinates in the lab frame in mm to pixels """
         if D is None:
             D = self.D
         Dinv = np.linalg.inv(D.double()).float()
         return (xyz@Dinv)[:,:2]
 
     def s12pix(self, s1, D=None):
+        """ Project a scattered beam wavevector into pixel coordinates. """
         if D is None:
             D = self.D
         Dinv = np.linalg.inv(D.double()).float()
@@ -126,6 +163,7 @@ def orthogonalization(a, b, c, alpha, beta, gamma):
     ], device=a.device)
 
 def mat_to_rot_xyz(R, deg=True):
+    """ Decompose a rotation matrix into euler angles """
     if R[2, 0] < 1:
         if R[2,0] > -1:
             rot_y = np.asin(-R[2, 0])
@@ -146,6 +184,7 @@ def mat_to_rot_xyz(R, deg=True):
     return rot_x, rot_y, rot_z
 
 def rot_xyz_to_mat(rot_x, rot_y, rot_z, deg=True):
+    """ Convert euler angles into a rotation matrix """
     if deg:
         rot_x = np.deg2rad(rot_x)
         rot_y = np.deg2rad(rot_y)
@@ -159,4 +198,170 @@ def rot_xyz_to_mat(rot_x, rot_y, rot_z, deg=True):
         [cy*sz, cx*cz+sx*sy*sz, -cz*sx+cx*sy*sz],
         [-sy, cy*sx, cx*cy],
     ], device=rot_x.device)
+
+class LaueAssigner():
+    """
+    This class can be used to assign miller indices to Laue data with 
+    poor initial geometry.
+    """
+    def __init__(self, s0, s1_obs, lam_min, lam_max, hmax, RB):
+        """
+        Parameters
+        ----------
+        s0 : array
+            The (potentially unnormalized) direction of the incoming beam wavevector.
+        s1_obs : array
+            The `n x 3` array of observed scattered beam wavevector directions. These will
+            be normalized. 
+        lam_min : float
+            The lower end of the wavelength range of the beam.
+        lam_max : float
+            The upper end of the wavelength range of the beam.
+        hmax : array
+            A vector [hmax, kmax, lmax] specifying the largest miller indices to be 
+            considered along each of the reciprocal basis vectors. 
+        RB : array
+            The approximate indexing solution. This matrix is called Astar some places, 
+            but it contains the reciprocal basis vectors as its *rows*. 
+            ```python
+            RB = np.array([
+                [astar_1, astar_2, astar_3], 
+                [bstar_1, bstar_2, bstar_3], 
+                [cstar_1, cstar_2, cstar_3], 
+            ])
+            ```
+            It is equal to an orthogonal matrix `R` times the matrix `B` which is related
+            to the orthogonalization matrix, `O`, by
+            ```python
+            B = np.linalg.inv(O).T
+            ```
+        """
+        self.s0 = np.array(s0) / np.linalg.norm(s0)
+        self.lam_min,self.lam_max = lam_min,lam_max
+        self.hmax = hmax
+        self.s1_obs = np.array(s1_obs) / np.linalg.norm(s1_obs, axis=1)[:,None]
+        self.H = None
+        self.RB = np.array(RB)
+        self.ewald_offset = None
+        self.L = None
+        self.assign()
+
+    def assign(self):
+        """
+        This function updates H with the current best miller indices by searching the entire
+        set of feasible reflections specified by self.lam_min, self.lam_max, self.hmax, and the 
+        current value of self.RB. It also updates self.ewald_offset which is used to weight 
+        the optimization objective. It will also set self.L to the current most probable
+        wavelengths.
+
+        Returns
+        -------
+        self : LaueAssigner
+            This method returns self to enable chaining.
+        """
+        s0 = self.s0
+        s1 = self.s1_obs
+        Q = (s1 - s0)
+        RB = self.RB
+        lam_min,lam_max = self.lam_min,self.lam_max
+
+        # This block generates the entire reciprocal grid and then removes infeasible reflections
+        # TODO: to save memory, write a cython version of this using a triply nested for-loop 
+        # which never instantiates the infeasible reflecitons
+        hmax,kmax,lmax = self.hmax
+        Hall = np.mgrid[
+            -hmax:hmax+1:1,
+            -kmax:kmax+1:1,
+            -lmax:lmax+1:1,
+        ].reshape((3, -1)).T
+        Hall = Hall[np.any(Hall!=0, axis=1)] #<-- remove 0,0,0
+        Hall = Hall[((Hall / self.hmax)**2.).sum(-1) <= 1.]
+
+        Qpred = (RB@Hall.T).T
+        feasible = (
+            (np.linalg.norm(Qpred + s0/lam_min, axis=1) < 1/lam_min) & 
+            (np.linalg.norm(Qpred + s0/lam_max, axis=1) > 1/lam_max)
+        ) 
+        Qpred = Qpred[feasible]
+        lpred = - 2. * (Qpred@(s0)) * np.linalg.norm(Qpred, axis=1)**-2.
+        Qlpred = Qpred*lpred[:,None]
+
+        # This block matches the predicted scattering vectors to the observed ones
+        # TODO: add some checks for redundant assignments. 
+        distmat = np.linalg.norm(Q[:,None,:] - Qlpred[None,:,:], axis=-1)
+        idx = np.argmin(distmat, axis=1)
+        H_best = Hall[feasible][idx]
+        self.ewald_offset = np.linalg.norm(Qlpred[idx] - Q, axis=1)
+        self.H = H_best
+        return self
+
+    def optimize_bases(self, rlp_radius=0.002, steps=10):
+        """
+        Use convex optimization to improve the quality of the indexing matrix, self.RB.
+        This method solves a weighted L1 norm mnimization problem 
+
+        ```python
+        minimize ||w*(k*Qpred.T - RB@H.T)||_1
+             st.  1./lam_max <= k <= 1./lam_min
+        ```
+        Where the weights, w, are
+        ```python
+        w = np.exp(-0.5*(ewald_offset/rlp_radius)**2.)
+        w /= w.mean()
+        ```
+        After optimizing the bases, this method will call `self.assign` to update 
+        `self.H` to be consistent with the new bases. 
+
+        Parameters
+        ----------
+        rlp_radius : float (optional)
+            An approximate value for the radius of the reciprocal lattice points in 
+            inverse angstroms. This is used for weighting the objective function.
+            A suitable default is `0.002`.
+        steps : int (optional)
+            The number of cycles of convex optimization to run. The default is 10 which 
+            seems sufficient. 
+
+        Returns
+        -------
+        self : LaueAssigner
+            This method returns self to enable chaining.
+        """
+        import cvxpy as cvx
+        s0 = self.s0
+        s1 = self.s1_obs
+        Q = (s1 - s0)
+        lam_min,lam_max = self.lam_min,self.lam_max
+
+        hkl_pred,r = self.H, self.ewald_offset
+
+        k = cvx.Variable(len(self.s1_obs))
+        RB = cvx.Variable((3,3))
+        cons = [
+            k >= 1./lam_max,
+            k <= 1./lam_min,
+        ]
+
+        losses = []
+        correct = []
+
+        for i in range(steps):
+            w = np.exp(-0.5*(r/rlp_radius)**2.)
+            w /= w.mean()
+            Qpred = RB@hkl_pred.T
+            Ql = cvx.multiply(Q, k[:,None]).T
+            resid = Qpred - Ql
+            loss = cvx.norm1(cvx.multiply(w[None,:], resid)) 
+            p = cvx.Problem(
+                cvx.Minimize(loss),
+                cons,
+            )
+            p.solve(solver='ECOS', verbose=False, max_iters=1000)
+            r = np.linalg.norm(Ql.value - Qpred.value, axis=0)
+            hkl_pred = (np.linalg.inv(RB.value)@Ql.value).T
+            hkl_pred = np.round(hkl_pred)
+            #TODO: implement early termination by checking loss.value for convergence
+
+        self.RB = RB.value
+        self.assign()
 
