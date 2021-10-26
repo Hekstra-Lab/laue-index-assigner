d30853ae417830314afb08df78383ac92446a29a
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2021-10-26 16:53:23 -0400

    current version of refinement script

--- refine.sh
+++ refine.sh
@@ -7,11 +7,14 @@ TUKEY_MULTIPLIER=0.
 
 dials.refine ${FILE_OUTPUT_TEMPLATE}multi_stills.* \
   refinement.reflections.weighting_strategy.override='stills' \
-  refinement.reflections.outlier.algorithm='tukey' \
-  refinement.reflections.outlier.tukey.iqr_multiplier=0. \
+  refinement.parameterisation.detector.fix='orientation' `# Consider fixing Distance` \
   refinement.parameterisation.beam.fix=all \
-  refinement.parameterisation.detector.fix=orientation `# Consider fixing Distance` \
+  refinement.parameterisation.auto_reduction.action='remove' \
+  refinement.reflections.outlier.algorithm='mcd' \
   output.log="${FILE_OUTPUT_TEMPLATE}dials.ultra_refined.log" \
   output.experiments="${FILE_OUTPUT_TEMPLATE}ultra_refined.expt" \
   output.reflections="${FILE_OUTPUT_TEMPLATE}ultra_refined.refl" 
+
+#  refinement.reflections.outlier.algorithm='tukey' \
+#  refinement.reflections.outlier.tukey.iqr_multiplier=0. \
 
