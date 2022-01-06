baa0ab3fa0a88ddfacf7c101958a2dc01c16b1c6
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2022-01-06 12:28:20 -0500

    Updated refinement script for fixing beam params

--- refine_stills_no_sb.sh
+++ refine_stills_no_sb.sh
@@ -6,19 +6,24 @@ TUKEY_MULTIPLIER=0.
 # tentatively fix wavelength + b-axis
 # turn this into stills before refinement
 
-dials.refine ${FILE_INPUT_TEMPLATE}${1} \
+dials.refine  ${FILE_INPUT_TEMPLATE}${1} ${FILE_INPUT_TEMPLATE}${2} \
   refinement.reflections.weighting_strategy.override='stills' \
-  refinement.parameterisation.beam.fix='in_spindle_plane' `# Consider fixing Distance` \
-  refinement.parameterisation.detector.fix='distance' `# Consider fixing Distance` \
+  refinement.parameterisation.beam.fix="*in_spindle_plane *out_spindle_plane" \
+  refinement.parameterisation.detector.fix='distance' \
   refinement.parameterisation.crystal.unit_cell.fix_list='real_space_a' \
-  refinement.parameterisation.beam.constraints.parameter='Mu2' \
-  refinement.reflections.outlier.algorithm='null' \
-  output.log="${FILE_OUTPUT_TEMPLATE}dials.${2}.log" \
-  output.experiments="${FILE_OUTPUT_TEMPLATE}${2}.expt" \
-  output.reflections="${FILE_OUTPUT_TEMPLATE}${2}.refl" 
+  refinery.engine='SparseLevMar' \
+  refinement.reflections.outlier.minimum_number_of_reflections=1 \
+  refinement.reflections.outlier.algorithm='mcd' \
+  refinement.reflections.outlier.separate_experiments='False' \
+  refinement.parameterisation.auto_reduction.action='remove' \
+  parameterisation.auto_reduction.min_nref_per_parameter=1 \
+  output.log="${FILE_OUTPUT_TEMPLATE}dials.${3}.log" \
+  output.experiments="${FILE_OUTPUT_TEMPLATE}${3}.expt" \
+  output.reflections="${FILE_OUTPUT_TEMPLATE}${3}.refl" 
 #  refinement.parameterisation.beam.fix=all \
 
-#  refinement.reflections.outlier.algorithm='mcd' \
+#  refinement.reflections.outlier.algorithm='null' \
+#  refinement.parameterisation.beam.constraints.parameter='Mu2' \
 #  refinement.reflections.outlier.algorithm='tukey' \
 #  refinement.reflections.outlier.tukey.iqr_multiplier=0. \
 
