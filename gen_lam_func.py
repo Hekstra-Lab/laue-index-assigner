5067b8b7d7eaa9f7818ee42f9fd871995ce07fc4
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2022-01-13 14:52:26 -0500

    Added kde and heatmap

--- gen_lam_func.py
+++ gen_lam_func.py
@@ -7,6 +7,10 @@ from dxtbx.model.experiment_list import ExperimentListFactory
 from dials.array_family.flex import reflection_table
 from dials.array_family import flex
 from matplotlib import pyplot as plt
+import scipy
+import seaborn as sns
+
+# TODO: Write function 
 
 # Load files
 expt_file = "dials_temp_files/ultra_refined.expt"
@@ -17,7 +21,7 @@ refls = reflection_table.from_file(refl_file)
 # Get rlps and normalize
 refls.map_centroids_to_reciprocal_space(elist)
 rlps = refls['rlp'].as_numpy_array()
-normed_rlps = rlps / np.linalg.norm(rlps, axis=1)[:, None]
+norms = np.linalg.norm(rlps, axis=1)
 
 # Get angle between rlp and beam vector
 z = elist[0].beam.get_unit_s0()
@@ -26,31 +30,29 @@ thetas = rs.utils.math.angle_between(rlps, z) # in radians
 # Get wavelength of rlp
 lams = refls['Wavelength'].as_numpy_array()
 
-# Plot data
-plt.hist(np.rad2deg(thetas), bins=100)
-plt.xlabel('Angle with beam direction (degs)')
-plt.ylabel('Number of reflections')
+# Relevant plot
+# TODO: Add circles
+x = norms*np.cos(np.deg2rad(thetas))
+y = norms*np.sin(np.deg2rad(thetas))
+plt.plot(x, y, '.k', alpha=0.1)
+plt.xlim(-1,0.1)
+plt.ylim(-0.1,1)
 plt.show()
 
-plt.hist(lams, bins=100)
-plt.xlabel('Wavelength (Angstroms)')
-plt.ylabel('Number of reflections')
-plt.show()
+# Fit with kernel density estimator
+train_data = np.asarray([x, y])
+kde = scipy.stats.gaussian_kde(train_data)
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.scatter3D(normed_rlps[:,0], normed_rlps[:,1], normed_rlps[:,2], color='green')
-plt.title('Normalized RLPs')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.scatter3D(rlps[:,0], rlps[:,1], rlps[:,2], color='green')
-plt.title('RLPs')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+# Heatmap results of kde
+n = 20
+x_lims = np.linspace(-1, 0.1, n)
+y_lims = np.linspace(-0.1, 1, n)
+dx = (x_lims[-1] - x_lims[0]) / n
+dy = (y_lims[-1] - y_lims[0]) / n
+results = np.zeros(shape=(len(x_lims), len(y_lims)))
+for i in range(len(x_lims)):
+    for j in range(len(y_lims)):
+        results[i,j] = kde.integrate_box((x_lims[i]-dx/2, y_lims[j]-dy/2), (x_lims[i]+dx/2, y_lims[j]+dy/2))
+ax = sns.heatmap(results, annot=True, xticklabels=x_lims, yticklabels=y_lims, vmin=np.min(results), vmax=np.max(results), cbar=True)
+ax.invert_yaxis()
 plt.show()
