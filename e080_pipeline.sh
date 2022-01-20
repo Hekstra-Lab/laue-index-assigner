bf7b377973308efebf98e63ee52afc4fba319e24
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2022-01-20 08:14:03 -0500

    This is already done in refinement script

--- e080_pipeline.sh
+++ e080_pipeline.sh
@@ -3,5 +3,4 @@ cctbx.python sequence_to_stills_no_sb.py dials_temp_files/refined_varying.*
 cctbx.python dials_assign.py
 cctbx.python assign_beams_to_stills.py dials_temp_files/optimized.expt dials_temp_files/optimized.refl dials_temp_files/multi
 bash refine_stills_no_sb.sh multi.expt multi.refl ultra_refined
-cctbx.python store_wavelengths.py
 cctbx.python gen_lam_func.py
