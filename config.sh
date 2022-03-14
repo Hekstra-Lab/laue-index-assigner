353835ae06c186ee638095b387458d77c622357e
rahewitt <Rick.Lira.Morgan@gmail.com>
2022-03-14 10:12:59 -0400

    Renamed env variable again and printed additional info to user

--- config.sh
+++ config.sh
@@ -1,20 +1,26 @@
 # Use to define environment variables for location of data
 
 # Get user input for variables
-read -p $'Enter the path to the directory containing the diffraction image files:\n' IMG_DIR
+read -p $'Enter the path to the directory containing the diffraction image files:\n' DIFF_IMG_DIR
 
 # Check that directory exists
-if [ ! -d "$IMG_DIR" ]; then
-  echo $IMG_DIR "does not exist. Rerun configuration and provide accurate input."
+if [ ! -d "$DIFF_IMG_DIR" ]; then
+  echo $DIFF_IMG_DIR "does not exist. Rerun configuration and provide accurate input."
   exit
 fi;
 
 # Check that directory contains mccd files
-count=$(ls -1 ${IMG_DIR}/*.mccd 2>/dev/null | wc -l)
+count=$(ls -1 ${DIFF_IMG_DIR}/*.mccd 2>/dev/null | wc -l)
 if [ $count == 0 ]; then
   echo "Directory exists but contains no .mccd files. Rerun configuration and provide accurate input."
   exit
 fi;
 
+# Check if bashrc already contains this env variable
+
+
 # Export environment variables
-echo "export IMG_DIR=$IMG_DIR" >> ~/.bashrc
+echo "export DIFF_IMG_DIR='$DIFF_IMG_DIR'" >> ~/.bashrc
+
+# Tell user to source bashrc
+echo "Please reload your shell for changes to take effect."
