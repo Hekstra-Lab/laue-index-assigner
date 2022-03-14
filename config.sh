9e3d901a2bb576e0628f772fc1226acb6435ad85
rahewitt <Rick.Lira.Morgan@gmail.com>
2022-03-14 09:58:52 -0400

    Renamed an env variable to better represent general use

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 # Use to define environment variables for location of data
 
 # Get user input for variables
-read -p $'Enter the path to the directory containing .mccd files for DHFR:\n' DHFRMCCDDIR
+read -p $'Enter the path to the directory containing the diffraction image files:\n' IMG_DIR
 
 # Check that directory exists
-if [ ! -d "$DHFRMCCDDIR" ]; then
-  echo $DHFRMCCDDIR "does not exist. Rerun configuration and provide accurate input."
+if [ ! -d "$IMG_DIR" ]; then
+  echo $IMG_DIR "does not exist. Rerun configuration and provide accurate input."
   exit
 fi;
 
 # Check that directory contains mccd files
-count=$(ls -1 ${DHFRMCCDDIR}/*.mccd 2>/dev/null | wc -l)
+count=$(ls -1 ${IMG_DIR}/*.mccd 2>/dev/null | wc -l)
 if [ $count == 0 ]; then
   echo "Directory exists but contains no .mccd files. Rerun configuration and provide accurate input."
   exit
 fi;
 
 # Export environment variables
-echo "export DHFRMCCDDIR=$DHFRMCCDDIR" >> ~/.bashrc
+echo "export IMG_DIR=$IMG_DIR" >> ~/.bashrc
