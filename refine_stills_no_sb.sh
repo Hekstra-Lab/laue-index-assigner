a410228a1d9561994549beee05085328db8b367e
rahewitt <Rick.Lira.Morgan@gmail.com>
2022-03-14 09:52:26 -0400

    Made paths get sourced from script directory instead of hardcoding

--- refine_stills_no_sb.sh
+++ refine_stills_no_sb.sh
@@ -1,5 +1,6 @@
-FILE_INPUT_TEMPLATE="/n/home04/rahewitt/laue_indexer/laue-index-assigner/dials_temp_files/"
-FILE_OUTPUT_TEMPLATE="/n/home04/rahewitt/laue_indexer/laue-index-assigner/dials_temp_files/"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+FILE_INPUT_TEMPLATE="${SCRIPT_DIR}/dials_temp_files/"
+FILE_OUTPUT_TEMPLATE="${SCRIPT_DIR}/dials_temp_files/"
 TUKEY_MULTIPLIER=0.
 # 1 unit cell axis is fixed in precognition (b-axis) (maybe tolerance for detector distance -- look into this)
 # det. dist./wavelength/unit cell axes (fix 2/3)
