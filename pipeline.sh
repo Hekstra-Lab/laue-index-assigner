64454d9aa5ce74319bf7671e19aa79313e500d83
rahewitt <Rick.Lira.Morgan@gmail.com>
2022-03-25 15:39:07 -0400

    Renamed to be dataset-agnostic

--- pipeline.sh
+++ pipeline.sh
@@ -5,3 +5,4 @@ cctbx.python assign_beams_to_stills.py dials_temp_files/optimized.expt dials_tem
 bash refine_initial.sh multi.expt multi.refl ultra_refined
 bash refine_remove_outliers.sh ultra_refined.expt ultra_refined.refl mega_ultra_refined
 cctbx.python gen_lam_func.py
+cctbx.python poly_predictor.py
