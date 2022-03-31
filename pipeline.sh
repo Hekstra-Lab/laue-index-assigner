eb1677e305b8ca7e1545884f631a82144e1ba480
rahewitt <Rick.Lira.Morgan@gmail.com>
2022-03-31 11:44:38 -0400

    Remove utility file from pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -4,5 +4,4 @@ cctbx.python dials_assign.py
 cctbx.python assign_beams_to_stills.py dials_temp_files/optimized.expt dials_temp_files/optimized.refl dials_temp_files/multi
 bash refine_initial.sh multi.expt multi.refl ultra_refined
 bash refine_remove_outliers.sh ultra_refined.expt ultra_refined.refl mega_ultra_refined
-cctbx.python gen_lam_func.py
 cctbx.python poly_predictor.py
