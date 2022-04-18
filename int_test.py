b6720fff929d69cd18720edfef34788370fb25b4
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2022-04-18 13:07:00 -0400

    Created experiment identifiers mapping and filtered outliers based on flags + misc fixes

--- int_test.py
+++ int_test.py
@@ -7,19 +7,21 @@ from dxtbx.model import ExperimentList
 from dxtbx.model.experiment_list import ExperimentListFactory
 import logging
 from tqdm import tqdm, trange
+from utils.refl_table_utils import gen_experiment_identifiers
 from IPython import embed
 
 #logging.basicConfig(level=logging.DEBUG)
 print("Load DIALS files")
-#elist = ExperimentList.from_file("works_experiments.expt") 
-#refls =flex.reflection_table.from_file("works_indexed.refl")
-#preds = flex.reflection_table.from_file("works_predicted.refl")
-#elist = ExperimentListFactory.from_json_file("ultra_refined.expt", check_format=False)
-#refls = flex.reflection_table.from_file("ultra_refined.refl")
-elist = ExperimentListFactory.from_json_file("mega_ultra_refined.expt", check_format=False)
-refls =flex.reflection_table.from_file("mega_ultra_refined.refl")
-preds = flex.reflection_table.from_file("predicted.refl")
+elist = ExperimentListFactory.from_json_file("dials_temp_files/mega_ultra_refined.expt", check_format=True)
+refls = flex.reflection_table.from_file("dials_temp_files/mega_ultra_refined.refl")
+preds = flex.reflection_table.from_file("dials_temp_files/predicted.refl")
 phil_file = "proc_sigb.phil"
+
+# Get only refined reflections
+idx = refls.get_flags(refls.flags.used_in_refinement)
+refls = refls.select(idx)
+refls.reset_ids()
+preds.reset_ids()
 
 print("Populating columns in predictions")
 preds['flags'] = flex.size_t(len(preds), 1)
@@ -43,11 +45,17 @@ refls["bbox"] = new_bb
 if "shoebox" in list(refls.keys()):
     del refls["shoebox"]
 
+print("Mapping reflections to experiment identifiers")
+refls = gen_experiment_identifiers(refls, elist)
+preds = gen_experiment_identifiers(preds, elist)
+
 print("Integrating reflections")
-new_refls = int_methods.integrate(phil_file, elist, refls, preds)
-embed()
+new_elist, new_refls = int_methods.integrate(phil_file, elist, refls, preds)
 
-print("Writing data")
+print("Writing experiment data.")
+new_elist.as_file('dials_temp_files/integrated.expt')
 
+print("Writing reflection data.")
+new_refls.as_file('dials_temp_files/integrated.refl')
 
 print("Finished!")
