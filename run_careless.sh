692c416cef5239a86874b36cd7b5f68065e2f4e7
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-06-06 12:15:42 -0400

    add careless merging script

---
+++ run_careless.sh
@@ -0,0 +1,20 @@
+out=merge
+mtz=unmerged.mtz
+mkdir $out
+
+BASE_ARGS=(
+  --iterations=30_000 
+  --gpu-id=0 
+  --refine-uncertainties 
+  --merge-half-datasets
+  --wavelength-key='wavelength' 
+  "xobs,yobs,wavelength,dHKL,BATCH" 
+)
+
+
+careless poly \
+  ${BASE_ARGS[@]} \
+  $mtz \
+  $out/dhfr
+
+
