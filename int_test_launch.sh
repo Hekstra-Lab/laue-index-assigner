be9ffc66a9fd3ad7d74bbb12f16f0c3e4baaac1f
Dennis Brookner <dbrookner@holylogin04.rc.fas.harvard.edu>
2022-09-29 15:28:24 -0400

    pipeline takes variables, updated instructions

--- int_test_launch.sh
+++ int_test_launch.sh
@@ -3,8 +3,12 @@
 #SBATCH -n 12         # 8 cores
 #SBATCH --mem 96G    # memory pool for all cores
 #SBATCH -t 1-0:00   # time (D-HH:MM)
-#SBATCH -o int_test_%j.log              # Standard output
-#SBATCH -e int_test_%j.err              # Standard error
+#SBATCH -o logs/int_test_%j.log              # Standard output
+#SBATCH -e logs/int_test_%j.err              # Standard error
+
+DIALS_PATH=${2:-'/n/hekstra_lab/people/brookner/dials-v3-11-1/dials_env.sh'}
+
+source $DIALS_PATH
 
 cctbx.python int_test.py $1
 cctbx.python stills2mtz.py $1
--- integrate_launch.sh
+++ integrate_launch.sh
@@ -3,7 +3,11 @@
 #SBATCH -n 12         # 8 cores
 #SBATCH --mem 96G    # memory pool for all cores
 #SBATCH -t 1-0:00   # time (D-HH:MM)
-#SBATCH -o integrate_%j.log              # Standard output
-#SBATCH -e integrate_%j.err              # Standard error
+#SBATCH -o logs/integrate_%j.log              # Standard output
+#SBATCH -e logs/integrate_%j.err              # Standard error
+
+DIALS_PATH=${2:-'/n/hekstra_lab/people/brookner/dials-v3-11-1/dials_env.sh'}
+
+source $DIALS_PATH
 
 cctbx.python integrate.py $1
