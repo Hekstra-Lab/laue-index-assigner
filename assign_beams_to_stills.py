62972dd9791582afa3c0cfd21cf939ee94e46d45
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-01-10 09:05:44 -0500

    Updated beam assignment algorithm

--- assign_beams_to_stills.py
+++ assign_beams_to_stills.py
@@ -1,6 +1,7 @@
 from dials.array_family import flex
 from dxtbx.model.experiment_list import ExperimentListFactory
 from dxtbx.model import ExperimentList
+from dials.algorithms.refinement.prediction.managed_predictors import ExperimentsPredictorFactory
 from copy import deepcopy
 from tqdm import tqdm, trange
 import numpy as np
@@ -40,26 +41,38 @@ dials_df = rs.DataSet({
  
 # Generate beams per reflection
 print(f'Number of rows: {len(dials_df)}')
+exp_id = -1
 for i, refl in tqdm(dials_df.iterrows()):
-    # New beam per reflection
-    expt = expts[refl['ID'][i]]
-    temp = expt.beam.get_s0()
-    new_expt = expt
-    new_expt.beam = deepcopy(expt.beam)
-    new_expt.beam.set_wavelength(refl['Wavelength'][i])
-    s0 = (expt.beam.get_s0() / np.linalg.norm(expt.beam.get_s0())) / new_expt.beam.get_wavelength()
-    new_expt.beam.set_s0(s0)
-    new_expt.identifier = str(i)
-    new_expts.append(new_expt)
+    try:
+        # New beam per reflection
+        expt = expts[refl['ID'][i]]
+        temp = expt.beam.get_s0()
+        new_expt = expt
+        new_expt.beam = deepcopy(expt.beam)
+        new_expt.beam.set_wavelength(refl['Wavelength'][i])
+        s0 = (expt.beam.get_s0() / np.linalg.norm(expt.beam.get_s0())) / new_expt.beam.get_wavelength()
+        new_expt.beam.set_s0(s0)
+        exp_id = exp_id + 1 # Increment experiment ID
+        new_expt.identifier = str(exp_id)
+        new_expts.append(new_expt)
 
-    # Write new beam identifiers to reflections
-    dials_df.at[i,'new_ID'] = i
+        # Write new beam identifiers to reflections
+        dials_df.at[i,'new_ID'] = exp_id
+    except:
+        print('Warning: Unindexed strong spot has wavelength 0.')
+        dials_df.at[i,'new_ID'] = -1
+        continue
 
 print('finished loop')
 # Replace reflection IDs with new IDs
 idx = flex.int(dials_df['new_ID'])
 refl_output["id"] = idx
 print('assigned IDs')
+
+# Repredict centroids
+print('Repredicting centroids')
+ref_predictor = ExperimentsPredictorFactory.from_experiments(new_expts)
+ref_predictor(refl_output)
 
 print('writing experiments')
 # Write experiment file with multiple beams
