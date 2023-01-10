b643d2025c5e31e8b03759394f9760a5e4159f7f
PrinceWalnut <Rick.Lira.Morgan@gmail.com>
2023-01-10 09:06:18 -0500

    Removed extraneous comments

--- refine_initial.sh
+++ refine_initial.sh
@@ -2,10 +2,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FILE_INPUT_TEMPLATE="${SCRIPT_DIR}/${4}/"
 FILE_OUTPUT_TEMPLATE="${SCRIPT_DIR}/${4}/"
 TUKEY_MULTIPLIER=0.
-# 1 unit cell axis is fixed in precognition (b-axis) (maybe tolerance for detector distance -- look into this)
-# det. dist./wavelength/unit cell axes (fix 2/3)
-# tentatively fix wavelength + b-axis
-# turn this into stills before refinement
 
 dials.refine -v -v  ${FILE_INPUT_TEMPLATE}${1} ${FILE_INPUT_TEMPLATE}${2} \
   refinement.reflections.weighting_strategy.override='stills' \
